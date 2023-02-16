94b05cf6246a23838abb0e00412524c28db34453
Hafflgav <52525.thomas@gmail.com>
2023-02-16 11:13:30 +0100

    style: disable theme switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,6 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 module.exports = {
@@ -103,7 +102,10 @@ module.exports = {
                     },],
                 },], copyright: `Copyright © ${new Date().getFullYear()} Miragon`,
             }, prism: {
-                theme: lightCodeTheme, darkTheme: darkCodeTheme,
+                theme: lightCodeTheme,
+            },
+            colorMode: {
+                disableSwitch: true,
             },
         }),
 };
