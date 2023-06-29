d45fd3221682b51ef4faf55d082849d041e6fd55
Rami Nakkar <rami.nakkar@miragon.io>
2023-06-29 13:32:48 +0200

    fix: require light theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
 
 /** @type {import('@docusaurus/types').Config} */
 module.exports = {
